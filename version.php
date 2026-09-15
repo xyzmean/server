8978c30d36322c8561b39b1db1746451e9b5ad3a
xyzmean <163691714+xyzmean@users.noreply.github.com>
2026-09-15 14:46:16 +0300

    chore: build number 1014

--- version.php
+++ version.php
@@ -12,7 +12,7 @@ declare(strict_types=1);
 // when updating major/minor version number.
 
 // Четвёртая цифра — номер сборки xcloud, а не апстрима: см. bump-version.sh
-$OC_Version = [34, 0, 3, 1013];
+$OC_Version = [34, 0, 3, 1014];
 
 // The human-readable string
 $OC_VersionString = '34.0.3';
